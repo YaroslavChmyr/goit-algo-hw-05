717b4e6ec487b8781ba804a9c0f01ae3bc10e44a
Yaroslav Chmyr <yaroslav.ch72@gmail.com>
2024-02-20 16:22:37 +0200

    Task two done.

---
+++ task_two.py
@@ -0,0 +1,40 @@
+def binary_search(arr, x):
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+    iterations = 0
+
+    while low <= high:
+
+        mid = (high + low) // 2
+        iterations += 1
+
+        if arr[mid] < x:
+            low = mid + 1
+
+        elif arr[mid] > x:
+            high = mid - 1
+
+        else:
+            return iterations, arr[mid]
+
+    if high < 0:
+        return iterations, None
+    else:
+        return iterations, arr[low] if low < len(arr) else None
+
+# Створюємо відсортований масив з дробовими числами та перевіряємо алгоритм на двох різних значеннях
+
+arr = [2.1, 3, 4.3, 10.5, 40.7]
+x = 10
+y = 2.5
+
+iterations, upper_bound = binary_search(arr, x)
+
+print("Кількість ітерацій:", iterations)
+print("Верхня межа:", upper_bound)
+
+iterations, upper_bound = binary_search(arr, y)
+
+print("\nКількість ітерацій:", iterations)
+print("Верхня межа:", upper_bound)
